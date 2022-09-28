9c208a0128fb1ddfba71a953a6861a2fed2f1f71
Juventino Neto <nettojuventino@gmail.com>
2022-09-28 10:49:35 -0300

    Integrating with telegram bot

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using CryptoBot.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<ScheduleService>();
+builder.Services.AddSingleton<IntegrationService>();
 
 var app = builder.Build();
 
---
+++ Services/IntegrationService.cs
@@ -0,0 +1,25 @@
+﻿using Flurl.Http;
+
+namespace CryptoBot.Services;
+
+public class IntegrationService
+{
+    private readonly IConfiguration _configuration;
+    private int _cont;
+    public IntegrationService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _cont = 0;
+    }
+
+    public async Task SendMessage()
+    {
+        var endpoint = _configuration.GetSection("Binance:ApiUrlBase").Value;
+        var teste = _configuration.GetSection("Telegram:Url").Value;
+
+        Console.WriteLine($"{_cont++} - {endpoint}");
+        var request = string.Format(teste, "Bot API token");
+
+        await request.PostJsonAsync(new { chat_id = -634037016, text = Resource.Regular_Report });
+    }
+}
